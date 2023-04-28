e7d73193d45fe32dc136f3d4dba16bbe8e4426eb
efoneli <abbaefoneli@gmail.com>
2023-04-28 14:31:26 +0100

    test

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
-function greet (person: string)  {
+function greet (person: string = 'stranger')  {
     return `Hi there ${person}`
 }
 
+const doSomething = ( person: string, age: number, isFunny: boolean) => {};
+
+greet('sarah baaggigs')
+
+doSomething('Sarah Baggins', 12, false,)
+
 greet()
+
+function square(num: number) {
+     num * num
+}
+
+square(4)

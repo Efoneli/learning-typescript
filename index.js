aa4a2c5b6e61bcb018396cf4c26c8dde77df49ee
efoneli <abbaefoneli@gmail.com>
2023-04-28 15:59:36 +0100

    test

---
+++ index.js
@@ -0,0 +1,36 @@
+// function greet (person: string = 'stranger')  {
+//     return `Hi there ${person}`
+// }
+// const doSomething = ( person: string, age: number, isFunny: boolean) => {};
+// greet('sarah baaggigs')
+// doSomething('Sarah Baggins', 12, false,)
+// greet()
+// function square(num: number):number {
+//      return num * num
+// }
+// square(4)
+// const add = (x: number, y: number): number => {
+//     return x + y;
+// };
+// function rand(num:number) {
+//     if(Math.random() < 0.5) {
+//         return num.toString();
+//     }
+//     return num;
+// }
+// const colors = ['red', 'orange', 'yellow'];
+// colors.map(color => {
+//     return color.toUpperCase()
+// })
+// function printTwice(msg: string): void {
+//     console.log(msg);
+//     console.log(msg);
+// }
+function twoFer(person) {
+    return "one for " + person + ", one for me.";
+}
+var isLeapYear = function (year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+console.log(isLeapYear(2012));
+console.log(twoFer('Pearsons'));
--- index.ts
+++ index.ts
@@ -1,38 +1,49 @@
-function greet (person: string = 'stranger')  {
-    return `Hi there ${person}`
+// function greet (person: string = 'stranger')  {
+//     return `Hi there ${person}`
+// }
+
+// const doSomething = ( person: string, age: number, isFunny: boolean) => {};
+
+// greet('sarah baaggigs')
+
+// doSomething('Sarah Baggins', 12, false,)
+
+// greet()
+
+// function square(num: number):number {
+//      return num * num
+// }
+
+// square(4)
+
+// const add = (x: number, y: number): number => {
+//     return x + y;
+// };
+
+// function rand(num:number) {
+//     if(Math.random() < 0.5) {
+//         return num.toString();
+//     }
+//     return num;
+// }
+
+// const colors = ['red', 'orange', 'yellow'];
+// colors.map(color => {
+//     return color.toUpperCase()
+// })
+
+// function printTwice(msg: string): void {
+//     console.log(msg);
+//     console.log(msg);
+// }
+
+function twoFer(person: string = 'you' ): string {
+    return `one for ${person}, one for me.`;
 }
+console.log(twoFer());
+console.log(twoFer('Pearsons'));
 
-const doSomething = ( person: string, age: number, isFunny: boolean) => {};
-
-greet('sarah baaggigs')
-
-doSomething('Sarah Baggins', 12, false,)
-
-greet()
-
-function square(num: number):number {
-     return num * num
+const isLeapYear= (year: number): boolean => {
+    return(year % 4 === 0 && year % 100 !== 0) ||  year % 400 === 0
 }
-
-square(4)
-
-const add = (x: number, y: number): number => {
-    return x + y;
-};
-
-function rand(num:number) {
-    if(Math.random() < 0.5) {
-        return num.toString();
-    }
-    return num;
-}
-
-const colors = ['red', 'orange', 'yellow'];
-colors.map(color => {
-    return color.toUpperCase()
-})
-
-function printTwice(msg: string): void {
-    console.log(msg);
-    console.log(msg);
-}
+console.log(isLeapYear(2012))

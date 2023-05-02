4e3d34f768bf2dfaeac06d9b62dd7c60fcdbf8d0
efoneli <abbaefoneli@gmail.com>
2023-05-02 16:28:55 +0100

    test

--- index.ts
+++ index.ts
@@ -207,47 +207,67 @@
 // console.log(getProfit(dune))
 // console.log(getProfit(cats))
 
-const actUSers: string[] = []
-actUSers.push('Tony')
+// const actUSers: string[] = []
+// actUSers.push('Tony')
 
-const noOfPipo: number[] = []
-noOfPipo.push(23)
+// const noOfPipo: number[] = []
+// noOfPipo.push(23)
 
-const bools: boolean[] = []
-
-
-type Point = {
-    x: number,
-    y: string
-}
-
-const testMe: Point [] = []
-testMe.push({x: 3, y: 'yamarita'})
-testMe.push({ x: 4, y:'flakes'})
-testMe.push({x: 3, y: 4})
+// const bools: boolean[] = []
 
 
-const boards: string[][] = [
-    ['X', 'O', 'X'],
-    ['X', 'O', 'X'],
-    ['X', 'O', 'X']
-]
+// type Point = {
+//     x: number,
+//     y: string
+// }
 
-const ages: number[] = []
-
-const gameBoard: string[][] = [];
-
-type Product = {
-    name: string,
-    price: number,
-}
+// const testMe: Point [] = []
+// testMe.push({x: 3, y: 'yamarita'})
+// testMe.push({ x: 4, y:'flakes'})
+// testMe.push({x: 3, y: 4})
 
 
-function getTotal(products: Product[] ): number {
-    let total = 0;
-    for (let product of products) {
-        total += product.price;
-    }
+// const boards: string[][] = [
+//     ['X', 'O', 'X'],
+//     ['X', 'O', 'X'],
+//     ['X', 'O', 'X']
+// ]
 
-    return total;
-}
+// const ages: number[] = []
+
+// const gameBoard: string[][] = [];
+
+// type Product = {
+//     name: string,
+//     price: number,
+// }
+
+
+// function getTotal(products: Product[] ): number {
+//     let total = 0;
+//     for (let product of products) {
+//         total += product.price;
+//     }
+
+//     return total;
+// }
+
+ type Point = {
+    x: number;
+    y: number; 
+ }
+
+ type Loc  = {
+    lat: number;
+    long: number;
+ }
+
+ let coordinates: Point | Loc = {x: 1, y: 34}
+ coordinates = { lat: 23.23, long: 24.34 }
+
+const coords: (Point | Loc)[] = []
+coords.push({lat: 23.44, long: 34.21})
+coords.push({x: 23.44, y: 34.21})
+
+let mood: 'Happy' | 'Sad' = 'Happy'
+mood = 'Sad'
